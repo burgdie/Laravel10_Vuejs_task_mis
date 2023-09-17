d8c866bb37579ed51586f57d7fd9ec40c4a2f244
Dieter Burgstaller <72778774+burgdie@users.noreply.github.com>
2023-09-17 17:33:34 +0200

    24 Adding Vue Js - S3 Vue JS Basics - Master Laravel with VueJs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 require('./bootstrap');
+import{createApp} from 'vue';
+const app = createApp({});
+
+app.mount('#app');
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,7 +17,7 @@
 
 <body>
 
-    <div class="wrapper d-flex align-items-stretch">
+    <div class="wrapper d-flex align-items-stretch" id="app">
         <nav id="sidebar">
             <div class="p-4 pt-5">
                 <a href="#" class="img logo rounded-circle mb-5">
