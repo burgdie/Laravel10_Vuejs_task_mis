d1b11eb24a163c7b23dc71249b7c5f8742e8a1bb
Dieter Burgstaller <72778774+burgdie@users.noreply.github.com>
2023-09-18 08:57:16 +0200

    27 Vue JS Method Properties - S3 Vue JS Basics - Master Laravel with VueJs

--- resources/js/components/Departments.vue
+++ resources/js/components/Departments.vue
@@ -3,9 +3,10 @@
     <h1>Departments List</h1>
 
     <!-- Button trigger modal -->
-    <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    <!-- <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
       Create New Department
-    </button>
+    </button> -->
+    <button class="btn btn-success" @click="createDepartment">New Department</button>
 
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -53,6 +54,11 @@
       return {
         name: '',
         director_id: '',
+      }
+    },
+    methods: {
+      createDepartment() {
+        $('#exampleModal').modal('show')
       }
     }
 
