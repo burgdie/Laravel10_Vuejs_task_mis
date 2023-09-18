fde8550b4a176302b40428d908df67cd0b694175
Dieter Burgstaller <72778774+burgdie@users.noreply.github.com>
2023-09-18 10:39:44 +0200

    29 Vue JS CRUD - S3 Vue JS Basics - Master Laravel with VueJs

--- resources/js/components/Departments.vue
+++ resources/js/components/Departments.vue
@@ -1,52 +1,56 @@
 <template>
-  
-    <h1>Departments List</h1>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header bg-dark">
+          <h5 class="float-start text-light">Departments List</h5>
+          <button class="btn btn-success float-end" @click="createDepartment">New Department</button>
 
-    <!-- Button trigger modal -->
-    <!-- <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
-      Create New Department
-    </button> -->
-    <button class="btn btn-success" @click="createDepartment">New Department</button>
+        </div>
+        <div class="card-body">
+      
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModal" tabindex="-1"     aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  ...
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" name="name" v-model="name">
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="director_id">Director</label>
+                            <select name="director_id" id="" class="form-control" v-model="director_id">
+                                <option value="" >Select a person</option>
+                                <option value="1">IT Director</option>
+                                <option value="2">HR Director</option>
 
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            ...
-            <div class="row">
-              <div class="col-md-6">
-                  <div class="form-group">
-                      <label for="name">Name</label>
-                      <input type="text" class="form-control" name="name" v-model="name">
+                            </select>
+                        </div>
+                    </div>
                   </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-success">Save changes</button>
+                </div>
+               
               </div>
-              <div class="col-md-6">
-                  <div class="form-group">
-                      <label for="director_id">Director</label>
-                      <select name="director_id" id="" class="form-control" v-model="director_id">
-                          <option value="" >Select a person</option>
-                          <option value="1">IT Director</option>
-                          <option value="2">HR Director</option>
-
-                      </select>
-                  </div>
-              </div>
-              </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>  
-
-  
+    </div>
+  </div>
 </template>
 <script >
   export default{
