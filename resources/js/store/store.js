35b6049d47c54b3692e38a7b62b6f452565e74b1
Dieter Burgstaller <72778774+burgdie@users.noreply.github.com>
2023-10-02 03:17:36 +0200

    46 Vuex  Modules - S5 Vue Js State Management - Master Laravel with Vue Js

--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -1,7 +1,12 @@
 import { createStore } from "vuex";
 
+import departmentsModule from './modules/departments';
+
 export const store = createStore({
   strict: true,
+  modules: {
+    departmentsModule
+  },
   state: {
     test: 0,
   },
