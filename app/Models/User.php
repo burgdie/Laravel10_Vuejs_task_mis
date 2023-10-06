8e116efcd0d42c57147785f8dc2a3e75a2c233be
Dieter Burgstaller <72778774+burgdie@users.noreply.github.com>
2023-10-06 11:41:00 +0200

    52 Laratrust Installation - S6 Authorization - Master Laravel With VueJs

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 // use Laravel\Sanctum\HasApiTokens;
 use Laravel\Passport\HasApiTokens;
+use Laratrust\Traits\LaratrustUserTrait;
 
 class User extends Authenticatable
 {
+    use LaratrustUserTrait;
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
